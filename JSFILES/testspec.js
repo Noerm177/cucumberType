c046c1cdcc0df931f6197a1c66b14d94e014d2c9
noerm <noerm177@gmail.com>
2019-02-01 00:56:25 -0600

    primer pageobject "calculadora"

--- JSFILES/testspec.js
+++ JSFILES/testspec.js
@@ -9,29 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const protractor_1 = require("protractor");
+const calculator_1 = require("./pageObjects/calculator");
 describe('Chain locat ors demo', () => {
-    /* it('Open Angular js website', async () => {
- 
- 
- 
-         await browser.get('http://juliemr.github.io/protractor-demo/');
-         //repeater ,  chain locators, And css for identical tags
-         //await calc.firstEditBox.sendKeys("3");
-         await calc.secondEditBox.sendKeys("5");
-         await calc.go.click();
-         calc.getResult.getText().then(function (text) {
-              console.log(text);
-         })
- 
- 
-         //	element(by.model("operator")).element(by.css("option:nth-child(4)")).click();
- 
- 
-     })*/
-    it('Angular hompe page', () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get('https://angularjs.org/');
-        yield protractor_1.element(protractor_1.by.linkText("angular.io")).click();
-        yield protractor_1.element(protractor_1.by.css("input[type='search']")).sendKeys('hello');
+    it('Open Angular js website', () => __awaiter(this, void 0, void 0, function* () {
+        let calc = new calculator_1.calculator();
+        yield protractor_1.browser.get('http://juliemr.github.io/protractor-demo/');
+        yield calc.firstEditBox.sendKeys("3");
+        yield calc.secondEditBox.sendKeys("2");
+        yield calc.go.click();
+        calc.getResult.getText().then(function (text) {
+            console.log(text);
+        });
+        //	element(by.model("operator")).element(by.css("option:nth-child(4)")).click();
     }));
+    /*it('Angular hompe page', async () =>
+    {
+
+     await   browser.get('https://angularjs.org/');
+     await   element(by.linkText("angular.io")).click();
+     await   element(by.css("input[type='search']")).sendKeys('hello');
+    });*/
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdHNwZWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90ZXN0c3BlYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsMkNBQStFO0FBRy9FLFFBQVEsQ0FBQyxzQkFBc0IsRUFBRSxHQUFHLEVBQUU7SUFHbkM7Ozs7Ozs7Ozs7Ozs7Ozs7O1NBaUJLO0lBRUosRUFBRSxDQUFDLG9CQUFvQixFQUFFLEdBQVMsRUFBRTtRQUduQyxNQUFRLG9CQUFPLENBQUMsR0FBRyxDQUFDLHdCQUF3QixDQUFDLENBQUM7UUFDOUMsTUFBUSxvQkFBTyxDQUFDLGVBQUUsQ0FBQyxRQUFRLENBQUMsWUFBWSxDQUFDLENBQUMsQ0FBQyxLQUFLLEVBQUUsQ0FBQztRQUNuRCxNQUFRLG9CQUFPLENBQUMsZUFBRSxDQUFDLEdBQUcsQ0FBQyxzQkFBc0IsQ0FBQyxDQUFDLENBQUMsUUFBUSxDQUFDLE9BQU8sQ0FBQyxDQUFDO0lBQ25FLENBQUMsQ0FBQSxDQUFDLENBQUM7QUFFUCxDQUFDLENBQUMsQ0FBQSJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdHNwZWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90ZXN0c3BlYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsMkNBQStFO0FBRS9FLHlEQUFzRDtBQUV0RCxRQUFRLENBQUMsc0JBQXNCLEVBQUUsR0FBRyxFQUFFO0lBR2xDLEVBQUUsQ0FBQyx5QkFBeUIsRUFBRSxHQUFTLEVBQUU7UUFFeEMsSUFBSSxJQUFJLEdBQUUsSUFBSSx1QkFBVSxFQUFFLENBQUM7UUFFM0IsTUFBTSxvQkFBTyxDQUFDLEdBQUcsQ0FBQywyQ0FBMkMsQ0FBQyxDQUFDO1FBQy9ELE1BQU0sSUFBSSxDQUFDLFlBQVksQ0FBQyxRQUFRLENBQUMsR0FBRyxDQUFDLENBQUM7UUFDdEMsTUFBTSxJQUFJLENBQUMsYUFBYSxDQUFDLFFBQVEsQ0FBQyxHQUFHLENBQUMsQ0FBQztRQUN2QyxNQUFNLElBQUksQ0FBQyxFQUFFLENBQUMsS0FBSyxFQUFFLENBQUM7UUFFakIsSUFBSSxDQUFDLFNBQVMsQ0FBQyxPQUFPLEVBQUUsQ0FBQyxJQUFJLENBQUMsVUFBUyxJQUFJO1lBRXRDLE9BQU8sQ0FBQyxHQUFHLENBQUMsSUFBSSxDQUFDLENBQUM7UUFDdkIsQ0FBQyxDQUFDLENBQUE7UUFLSixnRkFBZ0Y7SUFHcEYsQ0FBQyxDQUFBLENBQUMsQ0FBQztJQUVIOzs7Ozs7U0FNSztBQUVULENBQUMsQ0FBQyxDQUFBIn0=
