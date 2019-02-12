e9b0e9f15ae93a5ef951b3ca6fae869586d9e1db
noerm <noerm177@gmail.com>
2019-02-12 00:28:33 -0600

    espacios

--- stepDefinations/steps.ts
+++ stepDefinations/steps.ts
@@ -3,16 +3,16 @@ import { async } from "q";
 import { calculator } from '../pageObjects/calculator';
 import { browser } from "protractor";
 import { angularHomePage } from '../pageObjects/angularHomePage';
+
 let calc = new calculator();
 let ah = new angularHomePage
 
-    Given('I will navigate to {string} site', async (string)=> {
-      // Write code here that turns the phrase above into concrete actions
+    Given('I will navigate to {string} site', async (string)=> {     
       await browser.get('http://juliemr.github.io/protractor-demo/');
     });  
 
     Given('I will navigate to {string} page', async (string)=> {
-        // Write code here that turns the phrase above into concrete actions
+        
         if(string=="calc")
         {
           await browser.get('http://juliemr.github.io/protractor-demo/');
@@ -23,34 +23,29 @@ let ah = new angularHomePage
         }
       });
 
-      When('I clicked on header link', async ()=> {
-        // Write code here that turns the phrase above into concrete actions
+      When('I clicked on header link', async ()=> {       
         let ah = new angularHomePage
         await ah.angularLink.click();
       });
 
-      When('You will navigate to angular page', async ()=> {
-        // Write code here that turns the phrase above into concrete actions
+      When('You will navigate to angular page', async ()=> {     
         console.log("navigate to new page");
       });
 
-      Then('You will enter {string} in search box', async (string)=> {
-        // Write code here that turns the phrase above into concrete actions
+      Then('You will enter {string} in search box', async (string)=> {        
         await browser.sleep(3000);
         await ah.search.sendKeys(string);
       });
 
       When('I add two numbers {string} and {string}', async(string, string2) => {
-        // Write code here that turns the phrase above into concrete action
         await calc.firstEditBox.sendKeys(string);
         await calc.secondEditBox.sendKeys(string2);
       });
 
-      Then('The output displayed should be {string}', async(string)=> {
-        // Write code here that turns the phrase above into concrete actions
+      Then('The output displayed should be {string}', async(string)=> {      
         await calc.go.click();
         await calc.getResult.getText().then(function (text) {
-            console.log(text);
+          console.log(text);
         })
       });
 
